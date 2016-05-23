39a22aafbaab04b6e5bda0bcbd9aef7b9632f791
berluskuni <berluskuni@gmail.com>
2016-05-23 18:13:16 +0300

    rendereng html files

---
+++ students/views/groups.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+__author__ = 'berluskuni'
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def groups_list(request):
+    groups = (
+        {
+            'id': 1,
+            'name': u'МтМ-21',
+            'steward': u'Ганжела Сергій',
+        },
+        {
+            'id': 2,
+            'name': u'МтМ-22',
+            'steward': u'Ганжела Андрйій',
+        },
+        {
+            'id': 3,
+            'name': u'МтМ-23',
+            'steward': u'Ганжела Пётр',
+        }
+
+    )
+    return render(request, 'groups_list.html', {'groups': groups})
+
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
+
+
+def groups_add(request):
+    return HttpResponse('<h1>Group add form</h1>')
--- students/views/students.py
+++ students/views/students.py
@@ -1,8 +1,7 @@
-#-*- coding: utf-8 -*-
+# coding=utf-8
+__author__ = 'berluskuni'
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Views fo Students
 
 
 def students_list(request):
@@ -35,41 +34,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Students %s</h1>' % sid)
-
-
-# Views for Groups
-
-def groups_list(request):
-    groups = (
-        {
-            'id': 1,
-            'name': u'МтМ-21',
-            'steward': u'Ганжела Сергій',
-        },
-        {
-            'id': 2,
-            'name': u'МтМ-22',
-            'steward': u'Ганжела Андрйій',
-        },
-        {
-            'id': 3,
-            'name': u'МтМ-23',
-            'steward': u'Ганжела Пётр',
-        }
-
-    )
-    return render(request, 'groups_list.html', {'groups': groups})
-
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
-
-
-def groups_add(request):
-    return HttpResponse('<h1>Group add form</h1>')
-
-
